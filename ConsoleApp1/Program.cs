9f0bc26155415f12df24aadef7d71e32faa5f4c5
Chris <lehmanc844@gmail.com>
2026-09-09 14:32:49 -0400

    Add header comment and introduction lines

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
-﻿
+﻿/*
+* Name: Christian Michael Lehman
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
 Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Christian Michael Lehman.");
+Console.WriteLine("This is my first program at ETSU.");
